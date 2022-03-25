05190a3e1ca15ee5a99eff2d5b79e5759ca9fb1e
Breno Lopes <brenokos@gmail.com>
2022-03-25 14:58:15 -0300

    test

--- app/backend/src/middlewares/joi/matchs.ts
+++ app/backend/src/middlewares/joi/matchs.ts
@@ -4,8 +4,8 @@ import * as Joi from 'joi';
 const schema = Joi.object({
   homeTeam: Joi.number().min(1).required(),
   awayTeam: Joi.number().min(1).required(),
-  homeTeamGoals: Joi.number().min(0).required(),
-  awayTeamGoals: Joi.number().min(0).required(),
+  /*   homeTeamGoals: Joi.number().min(0).required(),
+  awayTeamGoals: Joi.number().min(0).required(), */
   inProgress: Joi.boolean(),
 });
 
