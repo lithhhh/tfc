724dc3cde0ef795746aec4d039b6df6b7b078d59
Breno Lopes <brenokos@gmail.com>
2022-06-23 20:41:59 -0300

    feat: add imports in middleware layer

---
+++ app/backend/src/api/middlewares/joi/index.ts
@@ -0,0 +1,13 @@
+import loginSchema from './login.schema';
+import matchSchema from './matchs.schema';
+import matchScoreSchema from './matchScore.schema';
+import tokenSchema from './token.schema';
+import validateInput from './validator';
+
+export {
+  loginSchema,
+  matchSchema,
+  tokenSchema,
+  matchScoreSchema,
+  validateInput,
+};
