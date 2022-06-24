00d2f8a187176be307c20e3118913d95bbcceb4e
Breno Lopes <brenokos@gmail.com>
2022-06-23 21:52:14 -0300

    refactor: fix import in index.d.ts

--- app/backend/src/types/index.d.ts
+++ app/backend/src/types/index.d.ts
@@ -1,4 +1,4 @@
-import { IId } from '../interfaces';
+import { IId } from '../app/interfaces';
 // para termos o acesso à criação de variáveis no Request
 // ref: https://stackoverflow.com/questions/44383387/typescript-error-property-user-does-not-exist-on-type-request
 // 'express-serve-static-core': interface aberta para extensões
