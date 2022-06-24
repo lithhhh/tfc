220fd7fe6932d37d6c8738ddfdab447fca3baade
Breno Lopes <brenokos@gmail.com>
2022-06-24 19:15:07 -0300

    feat: adapt service changes to controller (login)

--- app/backend/src/app/controllers/login.ts
+++ app/backend/src/app/controllers/login.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { IId } from '../interfaces';
 import { Login } from '../services';
 import { loginSchema, validateInput } from '../../api/middlewares/joi';
+import { statusCodes } from '../utils';
 
 export default class LoginController {
   private service: Login;
@@ -14,18 +15,16 @@ export default class LoginController {
 
   async login(req: Request, res: Response) {
     validateInput(loginSchema, req.body);
-    const { code, message, payload } = await this.service.login(req.body);
+    const payload = await this.service.login(req.body);
 
-    if (message) return res.status(code).json({ message });
-
-    return res.status(code).json(payload);
+    return res.status(statusCodes.OK).json(payload);
   }
 
   async loginValidate(req: Request, res: Response) {
     const { id } = req.id as IId;
 
-    const { code, role } = await this.service.validate(id);
+    const role = await this.service.validate(id);
 
-    return res.status(code).json(role);
+    return res.status(statusCodes.OK).json(role);
   }
 }
