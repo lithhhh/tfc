0b3e90c4897c4fb3d2c8727b81d8b0b287222b0f
Breno Lopes <brenokos@gmail.com>
2022-06-23 21:36:23 -0300

    refactor: login.ts moved to ./api/routes (route layer)

--- app/backend/src/api/routes/login.ts
+++ app/backend/src/api/routes/login.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { loginValidation, authToken } from '../middlewares';
-import { LoginController } from '../controllers';
+import { Auth } from '../middlewares';
+import { LoginController } from '../../app/controllers';
 
 export default class LoginRoute {
   public login: Router; // public para declarar no app.ts
@@ -16,13 +16,12 @@ export default class LoginRoute {
   private routes(): void {
     this.login.post(
       '/',
-      loginValidation,
       this.loginController.login,
     );
 
     this.login.get(
       '/validate',
-      authToken,
+      Auth.Auth,
       this.loginController.loginValidate,
     );
   }
