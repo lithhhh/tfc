191bbb56027759fb5e3d065e6ce046a0eb9c15e6
Breno Lopes <brenokos@gmail.com>
2022-06-24 14:30:32 -0300

    chore: import abstract class

--- app/backend/src/api/routes/index.ts
+++ app/backend/src/api/routes/index.ts
@@ -2,5 +2,6 @@ import LoginRoute from './login';
 import ClubsRoute from './clubs';
 import MatchRoute from './matchs';
 import LeaderboardRoute from './leaderboard';
+import Route from './abstract.route';
 
-export { LoginRoute, ClubsRoute, MatchRoute, LeaderboardRoute };
+export { LoginRoute, ClubsRoute, MatchRoute, LeaderboardRoute, Route };
