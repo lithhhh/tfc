f6a26dcd44ee0e97d006d9204c07fac79539a4ee
Breno Lopes <brenokos@gmail.com>
2022-06-23 21:31:47 -0300

    refactor: validate.ts moved, renamed to validate.jwt and modified for better usability (jwt layer)

---
+++ app/backend/src/api/middlewares/jwt/validate.jwt.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Response, Request } from 'express';
+import { tokenSchema, validateInput } from '../joi';
+import { DomainError, jwt, statusCodes, StatusMessage } from '../../../app/utils';
+
+export default {
+  onlyAdminAuth: (req: Request, _res: Response, _next: NextFunction) => {
+    const { authorization } = req.headers;
+
+    validateInput(tokenSchema, authorization);
+
+    const decoded = jwt.verifyToken(authorization as string);
+    if (decoded.role !== 'admin') {
+      throw new DomainError(statusCodes.UNAUTHORIZED, StatusMessage.UNAUTHORIZED);
+    }
+    req.id = decoded;
+
+    _next();
+  },
+
+  Auth: (req: Request, _res: Response, _next: NextFunction) => {
+    const { authorization } = req.headers;
+
+    validateInput(tokenSchema, authorization);
+
+    const decoded = jwt.verifyToken(authorization as string);
+
+    req.id = decoded;
+
+    _next();
+  },
+};
