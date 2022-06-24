a7737116f31d2a34ef25d4756c8566e2fbc1a6d9
Breno Lopes <brenokos@gmail.com>
2022-06-23 21:43:42 -0300

    chore: remove index.ts unused

--- app/backend/src/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-import LoginController from './login';
-import ClubsController from './clubs';
-import MatchController from './matchs';
-import LeaderboardController from './leaderboard';
-
-export { MatchController, LoginController, ClubsController, LeaderboardController };
