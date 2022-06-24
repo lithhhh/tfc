17fe3334fe2687d2b7b87c6cf46910d4fbd79e68
Breno Lopes <brenokos@gmail.com>
2022-06-23 21:34:39 -0300

    chore: add barrel import to route layer

---
+++ app/backend/src/api/routes/index.ts
@@ -0,0 +1,6 @@
+import LoginRoute from './login';
+import ClubsRoute from './clubs';
+import MatchRoute from './matchs';
+import LeaderboardRoute from './leaderboard';
+
+export { LoginRoute, ClubsRoute, MatchRoute, LeaderboardRoute };
