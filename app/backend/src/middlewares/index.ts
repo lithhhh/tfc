04eceaa9942e6f7f62e0459a6b8a5b48a7660484
Breno Lopes <brenokos@gmail.com>
2022-06-23 21:51:11 -0300

    chore: delete unused imports

--- app/backend/src/middlewares/index.ts
+++
@@ -1,7 +0,0 @@
-import loginValidation from './joi/login';
-import tokenValidation from './joi/token';
-import authToken from './jwt/validate';
-import matchValidation from './joi/matchs';
-import matchScoreValidation from './joi/matchScore';
-
-export { loginValidation, tokenValidation, authToken, matchValidation, matchScoreValidation };
--- app/backend/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import LoginRoute from './login';
-import ClubsRoute from './clubs';
-import MatchRoute from './matchs';
-import LeaderboardRoute from './leaderboard';
-
-export { LoginRoute, ClubsRoute, MatchRoute, LeaderboardRoute };
