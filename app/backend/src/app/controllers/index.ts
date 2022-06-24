8c8a5d7ee3db8f181dafdf04d57701c0fc2a97a2
Breno Lopes <brenokos@gmail.com>
2022-06-23 21:39:45 -0300

    chore: add imports to controller layer

---
+++ app/backend/src/app/controllers/index.ts
@@ -0,0 +1,6 @@
+import LoginController from './login';
+import ClubsController from './clubs';
+import MatchController from './matchs';
+import LeaderboardController from './leaderboard';
+
+export { MatchController, LoginController, ClubsController, LeaderboardController };
