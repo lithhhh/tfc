88f01c55847ae9710278ceb1956a1f8a25733977
Breno Lopes <brenokos@gmail.com>
2022-06-24 18:56:27 -0300

    feat: add some options for status messages

--- app/backend/src/app/utils/enums/statusMessage.ts
+++ app/backend/src/app/utils/enums/statusMessage.ts
@@ -1,6 +1,11 @@
 enum StatusMessage {
   INTERNAL_SERVER_ERROR = 'internal server error',
   UNAUTHORIZED = 'Unauthorized',
+  EQUAL_TEAMS = 'It is not possible to create a match with two equal teams',
+  TEAM_NOT_FOUND = 'There is no team with such id!',
+  INCORRECT_CREDENTIALS = 'Incorrect email or password',
+
+  OK = 'ok',
 }
 
 export default StatusMessage;
