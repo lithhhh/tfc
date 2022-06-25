81e8dc518d8116aa6046f2108782fe49d1cb7a42
Breno Lopes <brenokos@gmail.com>
2022-06-24 22:12:28 -0300

    feat: create sorting attribute

--- app/backend/src/app/utils/leaderboard.ts
+++ app/backend/src/app/utils/leaderboard.ts
@@ -1,3 +1,11 @@
 import { IMatchWithScore, ILeaderboard, ISortArray, IClub } from '../interfaces';
 
-export default class LeaderboardCreator {}
+export default class LeaderboardCreator {
+  private sorting: ISortArray[] = [
+    { fields: 'totalPoints', direction: 'desc' },
+    { fields: 'totalVictories', direction: 'desc' },
+    { fields: 'goalsBalance', direction: 'desc' },
+    { fields: 'goalsFavor', direction: 'desc' },
+    { fields: 'goalsOwn', direction: 'asc' },
+  ];
+}
