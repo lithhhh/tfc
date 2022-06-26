f7f55896d2dce0ae7682ddb34380df1b8507af43
Breno Lopes <brenokos@gmail.com>
2022-06-26 16:01:13 -0300

    feat: add ALREADY_EXISTS message in Enum statusMessage

--- app/backend/src/app/utils/enums/statusMessage.ts
+++ app/backend/src/app/utils/enums/statusMessage.ts
@@ -4,6 +4,7 @@ enum StatusMessage {
   EQUAL_TEAMS = 'It is not possible to create a match with two equal teams',
   TEAM_NOT_FOUND = 'There is no team with such id!',
   INCORRECT_CREDENTIALS = 'Incorrect email or password',
+  ALREADY_EXISTS = 'email already exists',
 
   OK = 'ok',
 }
